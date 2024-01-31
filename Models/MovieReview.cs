cb84a6d8dfc05813cbc1ad970746085aab50fe4d
Sean <131737726+Snance1985@users.noreply.github.com>
2024-01-30 18:02:34 -0800

    created MovieReview.cs and MovieSearch.cs models

--- Models/MovieReview.cs
+++ Models/MovieReview.cs
@@ -2,16 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace team_gung_ho_coders_backend.Models;
 
-public class Movie
+public class MovieReview
 {
-    public int MovieId { get; set; }
-    [Required(ErrorMessage = "A movie title is required.")]
-    [Display(Name = "Movie Title")]
-    public string? MovieTitle { get; set; }
+    public int MovieReviewId { get; set; }
     [Required(ErrorMessage = "A movie review is required."), StringLength(500, ErrorMessage = "Movie review length cannot exceed 500 characters.")]
     [Display(Name = "Movie Review")]
 
-    public string? MovieReview { get; set; }
+    public string? Description { get; set; }
     [Required(ErrorMessage = "A movie rating is required."), Range(1, 5, ErrorMessage = "You must enter a rating between 1 and 5.")]
     [Display(Name = "Movie Rating")]
 
