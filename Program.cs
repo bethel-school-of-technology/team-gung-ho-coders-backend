f1e758d0aa3cffb72c06b5c91646cc095c934777
Aceviii <133720214+Aceviii@users.noreply.github.com>
2024-02-03 23:19:59 -0500

    migrated to sqlite

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using System.Text;
+using team_gung_ho_coders_backend.Migrations;
+
+using team_gung_ho_coders_backend.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSqlite<MovieDbContext>("Data Source=team_gung_ho_coders_backend.db");
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
